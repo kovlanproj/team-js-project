fe21d4d1baf4b7648fd478be6edd22aaabdc5898
Roman <roman.baytala@gmail.com>
2022-10-29 21:28:49 +0300

    scroll-to-top pagination darkMode popular films-loader
    там файл createFilmCardMarkup.js перероблено в main.js наразі не видаляв

---
+++ src/js/switcher.js
@@ -0,0 +1,28 @@
+const bodyRef = document.querySelector('body');
+const darkTheme = document.querySelector('.theme-switch-toggle');
+
+if (localStorage.getItem('darkMode') === null) {
+    localStorage.setItem('darkMode', "false");
+};
+
+checkDarkModeStatus();
+
+function checkDarkModeStatus() {
+    if (localStorage.getItem('darkMode') === "true") {
+        darkTheme.checked = true;
+        bodyRef.classList.add('dark');
+    } else {
+        darkTheme.checked = false;
+        bodyRef.classList.remove('dark');
+    };
+};
+
+function changeStatus() {
+    if (localStorage.getItem('darkMode') === "true") {
+        localStorage.setItem('darkMode', "false");
+        bodyRef.classList.remove('dark');
+    } else {
+        localStorage.setItem('darkMode', "true");
+        bodyRef.classList.add('dark');
+    };
+};
