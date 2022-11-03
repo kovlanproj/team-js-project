fca48b61af276ff8505f39b44795778cf7e19ecd
kovlanproj <kozorizva@gmail.com>
2022-11-03 23:05:57 +0200

    commit 2

--- src/js/showTrailer.js
+++ src/js/showTrailer.js
@@ -23,12 +23,13 @@ export function onPosterClick(e) {
 export function showTrailer() {
   refs.trailerBackdrop.classList.remove('is-hidden');
   window.addEventListener('keydown', onPressESC);
-  // document.querySelector('body').style.overflow = 'hidden';
+  document.querySelector('body').style.overflow = 'hidden';
 }
 
 function onCloseTrailer() {
   refs.trailerBackdrop.classList.add('is-hidden');
   window.removeEventListener('keydown', onPressESC);
+  document.querySelector('body').style.overflow = 'visible';
 }
 
 function onBackdropClick(e) {
