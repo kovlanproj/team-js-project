1dc4171bb8d65733b09ce16d0bd2baf86fda2414
kovlanproj <104130825+kovlanproj@users.noreply.github.com>
2022-11-10 08:28:07 +0000

    Deploying to gh-pages from @ kovlanproj/team-js-project@e67cd8c46d49153fa857b114841e75294e7d5049 🚀

---
+++ index.9e8f678b.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".back_to_top");window.addEventListener("scroll",(()=>{var t=window.pageYOffset,e=document.documentElement.clientHeight;t>e/2&&o.classList.add("back_to_top-show"),t<e/2&&o.classList.remove("back_to_top-show")})),o.addEventListener("click",(()=>{window.pageYOffset>0&&window.scrollTo({top:0,left:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.9e8f678b.js.map
